c1626322c0f164ce816577eba6397fb4ea400da8
Mark Agluba <mark.agluba@dawsoncollege.qc.ca>
2022-08-31 22:51:04 -0400

    trying to do the assignment but not showing errors

--- PianoSimulation/CircularArray.cs
+++ PianoSimulation/CircularArray.cs
@@ -1,9 +1,25 @@
 namespace PianoSimulation
 {
-    public class CircularArray:IRingBuffer
-    {   private double[] arr;
+    public class CircularArray:IRingBuffer {   
+        private double[] arr;
+
+        public CircularArray(int arrLength) {
+            arr = new Double[arrLength];
+        }
         public int Length() {
-            get => arr.Length;
+            get {arr.Length;}
+        }
+
+        public double Shift(double value) {
+
+        }
+
+        public double this[int index] {
+            get;
+        }
+
+        public void Fill(double[] array) {
+
         }
         
     }
