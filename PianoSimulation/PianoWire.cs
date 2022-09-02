5578e5c31dba4f5e2e2dc442534a3f79956279b0
Mark Agluba <mark.agluba@dawsoncollege.qc.ca>
2022-09-01 22:56:59 -0400

    did the methods for PianoWire but not tested yet

--- PianoSimulation/PianoWire.cs
+++ PianoSimulation/PianoWire.cs
@@ -8,7 +8,39 @@ namespace PianoSimulation
     public class PianoWire:IMusicalString
     {
         CircularArray CircArr;
-        double NoteFreq;
-        double SampleRate;
+        private double _NoteFreq;
+        private int _SampleRate;
+
+        public PianoWire(double NoteFreq, int SampleRate) {
+            _NoteFreq = NoteFreq;
+            _SampleRate = SampleRate;
+        }
+
+        public double NoteFrequency {
+            get {
+                return _NoteFreq;
+            }
+
+        }
+
+        public int NumberOfSamples {
+            get {
+                return _SampleRate;
+            }
+        }
+
+        public void Strike() {
+            Random rand = new Random();
+            double MIN_VALUE = -0.5;
+            double MAX_VALUE = 0.5;
+            for (int i = 0; i < CircArr.Length;i++) {
+                CircArr[i] = rand.NextDouble() * (MAX_VALUE - MIN_VALUE) + MIN_VALUE;
+            }
+        }
+
+        public double Sample(double decay=0.996) {
+            double newValue = ((CircArr[0] + CircArr[1]) / 2) * decay;
+            return CircArr.Shift(newValue);
+        }
     }
 }
