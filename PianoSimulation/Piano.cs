22773bf6374392d66362cc09df075e3ada7e4b65
Mark Agluba <mark.agluba@dawsoncollege.qc.ca>
2022-09-03 14:29:38 -0400

    starting the Piano class

--- PianoSimulation/Piano.cs
+++ PianoSimulation/Piano.cs
@@ -7,8 +7,30 @@ namespace PianoSimulation
 {
     public class Piano:IPiano {
         private List<IMusicalString> _pianoWires;
+        private string _keys;
         public Piano(string keys = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ",  int samplingRate = 44100) {
-            
+            _keys = keys;
+        }
+
+        public void StrikeKey(char key) {
+
+        }
+
+        public double Play() {
+            double play = 0.0;
+            return play;
+        }
+
+        public List<string> GetPianoKeys() {
+            List<string> pianoKeys = new List<string>();
+
+            return pianoKeys;
+        }
+
+        public string Keys {
+            get {
+                return _keys;
+            }
         }
     }
 }
