b14541e3f5b233c6d2c1a085d0831df8dd3c23a5
Mark Agluba <mark.agluba@dawsoncollege.qc.ca>
2022-09-03 13:45:23 -0400

    starting the piano class

---
+++ PianoSimulation/Piano.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PianoSimulation
+{
+    public class Piano:IPiano {
+        private List<IMusicalString> _pianoWires;
+        public Piano(string keys = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ",  int samplingRate = 44100) {
+            
+        }
+    }
+}
