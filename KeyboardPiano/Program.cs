9c099172c9931d79b87bbb38e71b18257de1b518
Mark Agluba <mark.agluba@dawsoncollege.qc.ca>
2022-09-11 19:23:52 -0400

    fixing the main menu for the piano

--- KeyboardPiano/Program.cs
+++ KeyboardPiano/Program.cs
@@ -29,278 +29,31 @@ namespace KeyboardPiano
 
                     }
                     else if (keyInfo.Key == ConsoleKey.Enter) {
-                        Console.WriteLine("You can now play the piano. You can go back to the menu by pressing 0.");
-                        ConsoleKeyInfo chinput = Console.ReadKey(true);
-                        switch (chinput.Key)
-                        {
-                            case ConsoleKey.Q:{
-                                piano.StrikeKey(chinput.KeyChar);
-                                for (int i = 0; i < 44100 * 3; i++) {
-                                    audio.Play(piano.Play());
-                                }
-                                break;
-                            }
-                            case ConsoleKey.D2:{
-                                piano.StrikeKey(chinput.KeyChar);
-                                for (int i = 0; i < 44100 * 3; i++) {
-                                    audio.Play(piano.Play());
-                                }
-                                break;
-                            }
-                            case ConsoleKey.W:{
-                                piano.StrikeKey(chinput.KeyChar);
-                                for (int i = 0; i < 44100 * 3; i++) {
-                                    audio.Play(piano.Play());
-                                }
-                                break;
-                            }case ConsoleKey.E:{
-                                piano.StrikeKey(chinput.KeyChar);
-                                for (int i = 0; i < 44100 * 3; i++) {
-                                    audio.Play(piano.Play());
-                                }
-                                break;
-                            }
-                            case ConsoleKey.D4:{
-                                piano.StrikeKey(chinput.KeyChar);
-                                for (int i = 0; i < 44100 * 3; i++) {
-                                    audio.Play(piano.Play());
-                                }
-                                break;
-                            }
-                            case ConsoleKey.R:{
-                                piano.StrikeKey(chinput.KeyChar);
-                                for (int i = 0; i < 44100 * 3; i++) {
-                                    audio.Play(piano.Play());
-                                }
-                                break;
-                            }
-                            case ConsoleKey.D5:{
-                                piano.StrikeKey(chinput.KeyChar);
-                                for (int i = 0; i < 44100 * 3; i++) {
-                                    audio.Play(piano.Play());
-                                }
-                                break;
-                            }
-                            case ConsoleKey.T:{
-                                piano.StrikeKey(chinput.KeyChar);
-                                for (int i = 0; i < 44100 * 3; i++) {
-                                    audio.Play(piano.Play());
-                                }
-                                break;
-                            }
-                            case ConsoleKey.Y:{
-                                piano.StrikeKey(chinput.KeyChar);
-                                for (int i = 0; i < 44100 * 3; i++) {
-                                    audio.Play(piano.Play());
-                                }
-                                break;
-                            }
-                            case ConsoleKey.D7:{
-                                piano.StrikeKey(chinput.KeyChar);
-                                for (int i = 0; i < 44100 * 3; i++) {
-                                    audio.Play(piano.Play());
-                                }
-                                break;
-                            }
-                            case ConsoleKey.U:{
-                                piano.StrikeKey(chinput.KeyChar);
-                                for (int i = 0; i < 44100 * 3; i++) {
-                                    audio.Play(piano.Play());
-                                }
-                                break;
-                            }
-                            case ConsoleKey.D8:{
-                                piano.StrikeKey(chinput.KeyChar);
-                                for (int i = 0; i < 44100 * 3; i++) {
-                                    audio.Play(piano.Play());
-                                }
-                                break;
-                            }
-                            case ConsoleKey.I:{
-                                piano.StrikeKey(chinput.KeyChar);
-                                for (int i = 0; i < 44100 * 3; i++) {
-                                    audio.Play(piano.Play());
-                                }
-                                break;
-                            }
-                            case ConsoleKey.D9:{
-                                piano.StrikeKey(chinput.KeyChar);
-                                for (int i = 0; i < 44100 * 3; i++) {
-                                    audio.Play(piano.Play());
-                                }
-                                break;
-                            }
-                            case ConsoleKey.O:{
-                                piano.StrikeKey(chinput.KeyChar);
-                                for (int i = 0; i < 44100 * 3; i++) {
-                                    audio.Play(piano.Play());
-                                }
-                                break;
-                            }
-                            case ConsoleKey.P:{
-                                piano.StrikeKey(chinput.KeyChar);
-                                for (int i = 0; i < 44100 * 3; i++) {
-                                    audio.Play(piano.Play());
-                                }
-                                break;
-                            }
-                            case ConsoleKey.Subtract:{
-                                piano.StrikeKey(chinput.KeyChar);
-                                for (int i = 0; i < 44100 * 3; i++) {
-                                    audio.Play(piano.Play());
-                                }
-                                break;
-                            }
-                            case ConsoleKey.Oem4:{
-                                piano.StrikeKey(chinput.KeyChar);
-                                for (int i = 0; i < 44100 * 3; i++) {
-                                    audio.Play(piano.Play());
-                                }
-                                break;
-                            }
-                            case ConsoleKey.OemPlus:{
-                                piano.StrikeKey(chinput.KeyChar);
-                                for (int i = 0; i < 44100 * 3; i++) {
-                                    audio.Play(piano.Play());
-                                }
-                                break;
-                            }
-                            case ConsoleKey.Z:{
-                                piano.StrikeKey(chinput.KeyChar);
-                                for (int i = 0; i < 44100 * 3; i++) {
-                                    audio.Play(piano.Play());
-                                }
-                                break;
-                            }
-                            case ConsoleKey.X:{
-                                piano.StrikeKey(chinput.KeyChar);
-                                for (int i = 0; i < 44100 * 3; i++) {
-                                    audio.Play(piano.Play());
-                                }
-                                break;
-                            }
-                            case ConsoleKey.D:{
-                                piano.StrikeKey(chinput.KeyChar);
-                                for (int i = 0; i < 44100 * 3; i++) {
-                                    audio.Play(piano.Play());
-                                }
-                                break;
-                            }
-                            case ConsoleKey.C:{
-                                piano.StrikeKey(chinput.KeyChar);
-                                for (int i = 0; i < 44100 * 3; i++) {
-                                    audio.Play(piano.Play());
-                                }
-                                break;
-                            }
-                            case ConsoleKey.F:{
-                                piano.StrikeKey(chinput.KeyChar);
-                                for (int i = 0; i < 44100 * 3; i++) {
-                                    audio.Play(piano.Play());
-                                }
-                                break;
-                            }
-                            case ConsoleKey.V:{
-                                piano.StrikeKey(chinput.KeyChar);
-                                for (int i = 0; i < 44100 * 3; i++) {
-                                    audio.Play(piano.Play());
-                                }
-                                break;
-                            }
-                            case ConsoleKey.G:{
-                                piano.StrikeKey(chinput.KeyChar);
-                                for (int i = 0; i < 44100 * 3; i++) {
-                                    audio.Play(piano.Play());
-                                }
-                                break;
-                            }
-                            case ConsoleKey.B:{
-                                piano.StrikeKey(chinput.KeyChar);
-                                for (int i = 0; i < 44100 * 3; i++) {
-                                    audio.Play(piano.Play());
-                                }
-                                break;
-                            }
-                            case ConsoleKey.N:{
-                                piano.StrikeKey(chinput.KeyChar);
-                                for (int i = 0; i < 44100 * 3; i++) {
-                                    audio.Play(piano.Play());
-                                }
-                                break;
-                            }
-                            case ConsoleKey.J:{
-                                piano.StrikeKey(chinput.KeyChar);
-                                for (int i = 0; i < 44100 * 3; i++) {
-                                    audio.Play(piano.Play());
-                                }
-                                break;
-                            }
-                            case ConsoleKey.M:{
-                                piano.StrikeKey(chinput.KeyChar);
-                                for (int i = 0; i < 44100 * 3; i++) {
-                                    audio.Play(piano.Play());
-                                }
-                                break;
-                            }
-                            case ConsoleKey.K:{
-                                piano.StrikeKey(chinput.KeyChar);
-                                for (int i = 0; i < 44100 * 3; i++) {
-                                    audio.Play(piano.Play());
-                                }
-                                break;
-                            }
-                            case ConsoleKey.OemComma:{
-                                piano.StrikeKey(chinput.KeyChar);
-                                for (int i = 0; i < 44100 * 3; i++) {
-                                    audio.Play(piano.Play());
-                                }
-                                break;
-                            }
-                            case ConsoleKey.OemPeriod:{
-                                piano.StrikeKey(chinput.KeyChar);
-                                for (int i = 0; i < 44100 * 3; i++) {
-                                    audio.Play(piano.Play());
-                                }
-                                break;
-                            }
-                            case ConsoleKey.Oem1:{
-                                piano.StrikeKey(chinput.KeyChar);
-                                for (int i = 0; i < 44100 * 3; i++) {
-                                    audio.Play(piano.Play());
-                                }
-                                break;
-                            }
-                            case ConsoleKey.Divide:{
-                                piano.StrikeKey(chinput.KeyChar);
-                                for (int i = 0; i < 44100 * 3; i++) {
-                                    audio.Play(piano.Play());
-                                }
-                                break;
-                            }
-                            case ConsoleKey.Oem8:{
-                                piano.StrikeKey(chinput.KeyChar);
-                                for (int i = 0; i < 44100 * 3; i++) {
-                                    audio.Play(piano.Play());
-                                }
-                                break;
-                            }
-                            case ConsoleKey.Spacebar:{
-                                piano.StrikeKey(chinput.KeyChar);
-                                for (int i = 0; i < 44100 * 3; i++) {
-                                    audio.Play(piano.Play());
-                                }
-                                break;
-                            }
-                            case ConsoleKey.D0: {
-                                isTrue = false;
-                                break;
+                        bool playPiano = true;
+                        do {
+                            try {
+                                Console.WriteLine("You can now play the piano. What key(s) would you like to play? Press 0 to escape.");
+                                string userInput = Console.ReadLine();
+                                foreach(char chinput in userInput) {
 
-                            }
-                            default:
-                                Console.WriteLine("Invalid input");
-                                break;
-                        }
+                                    if (chinput == '0') {
+                                        Console.WriteLine("You are now returning to the main menu.");
+                                        playPiano = false; 
+                                    }
+                                    else {
+                                        piano.StrikeKey(chinput);
+                                        for (int i = 0; i < 44100 * 3; i++) {
+                                            audio.Play(piano.Play());
+                                        }
 
+                                    }
+                                }
+                            }
+                            catch (ArgumentOutOfRangeException) {
+                                Console.WriteLine("You made an error! You pressed a key that is not in the piano. Try again.");
+                            }
+                        }while(playPiano);
+                        isTrue = false;
                     }
                     else  if (keyInfo.Key == ConsoleKey.Escape) {
                         Console.WriteLine("You have exited the program! GoodBye!");
